7fdd316d3017e39f62126c0628b83e2605b3addd
Caio Rodrigues <caio.rod18@gmail.com>
2018-01-18 17:37:05 -0200

    [FIX] get with api and basic components

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="corpo">
+    <meu-menu :rotas="routes"></meu-menu>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition> 
+  </div>
+</template>
+
+<script>
+
+import {routes} from './routes';
+import Menu from './componentes/shared/menu/Menu.vue'
+
+export default {
+
+  components : {
+    'meu-menu' : Menu
+  },
+
+  data() {
+
+    return {
+
+      routes
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+  .corpo {
+    font-family : Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .pagina-enter, .painel-fade-leave-active {
+      opacity: 0
+  }
+
+  .pagina-enter-active, .painel-fade-leave-active {
+      transition: opacity .7s;
+  }
+</style>
---
+++ src/componentes/home/Home.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+      <h1 class="centralizado"> {{ titulo }}</h1>
+      <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do titulo">
+      <ul class="lista-fotos">
+        <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+              <meu-painel :titulo="foto.titulo">
+                <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
+                <meu-botao
+                  tipo="button"
+                  rotulo="REMOVER" 
+                  @botaoAtivado="remove(foto)"
+                  :confirmacao="true"
+                  estilo="padrao">
+                </meu-botao>
+              </meu-painel>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import Painel from '../shared/painel/Painel.vue';
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Botao from '../shared/botao/Botao.vue';
+
+export default {
+
+  components: {
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao': Botao
+  },
+
+  data () {
+    return {
+      titulo: 'Alurapic',
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed: {
+
+    fotosComFiltro () {
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      }else {
+        return this.fotos;
+      }
+    }
+
+  },
+
+  methods: {
+
+      remove(foto) {
+            alert("Foto removida!")
+      }
+  },
+
+  created() {
+
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+}
+</script>
+
+<style lang="scss">
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+</style>
